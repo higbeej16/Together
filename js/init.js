d03d845ce2bc4f628afb84bce509c46961aecfa0
higbeej16 <80486404+higbeej16@users.noreply.github.com>
2021-12-19 12:34:25 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -264,7 +264,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 	animateSpan.typed({
-		strings: ["Sound Supervisor, Composer, Sound Designer, Re-Recording Mixer"],
+		strings: ["Sound Supervisor", "Composer", "Sound Designer", "Re-Recording Mixer"],
 		loop: true,
 		startDelay: 1e3,
 		backDelay: 2e3
